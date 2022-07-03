769a1e3458abef236d228ea7df50d97fc9869d84
Nelson Izah <izah_nelson@outlook.com>
2022-07-03 23:39:03 +0100

    Commiting Spotify Clone with React

---
+++ src/utils/Constant.js
@@ -0,0 +1,8 @@
+const reducerCases = {
+    SET_TOKEN: "SET_TOKEN",
+    SET_PLAYLISTS: "SET_PLAYLISTS",
+    SET_USER: "SET_USER",
+    SET_PLAYLIST: "SET_PLAYLIST",
+}
+
+export default reducerCases;
